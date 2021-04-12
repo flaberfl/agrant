c8c8d4c770d34167d68d252516576bd955d56dc4
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2021-04-12 23:04:31 +0300

    upd

---
+++ js/app.js
@@ -0,0 +1,30 @@
+const burger = document.getElementById('burger');
+const nav = document.getElementById('mobile-menu');
+const body = document.body;
+
+burger.addEventListener('click', event => {
+  if (body.classList.contains('show-sidebar')) {
+    closeSidebar();
+  } else {
+    showSidebar();
+  }
+});
+
+nav.addEventListener('click', event => {
+  closeSidebar();
+})
+
+function showSidebar() {
+  let mask = document.createElement('div');
+
+  mask.classList.add('mask');
+  mask.addEventListener('click', closeSidebar);
+  body.appendChild(mask);
+  
+  body.classList.add('show-sidebar');
+}
+
+function closeSidebar() {
+  body.classList.remove('show-sidebar');
+  document.querySelector('.mask').remove();
+}
